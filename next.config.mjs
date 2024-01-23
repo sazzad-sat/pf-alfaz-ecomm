5dc6b32d557c8b6cef0257c93ba8fdf724966016
Sazzad Ahmed <sazzad.sat@gmail.com>
2024-01-23 10:54:12 +0600

    Initial Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{
+      protocol: "https",
+      port: '',
+      hostname: 'picsum.photos'
+    }]
+  }
+};
 
 export default nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,6 +12,25 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "primary": {
+          "50": "#fff4ed",
+          "100": "#ffe6d5",
+          "200": "#feccaa",
+          "300": "#fdac74",
+          "400": "#fb8a3c",
+          "500": "#f97316",
+          "600": "#ea670c",
+          "700": "#c2570c",
+          "800": "#9a4a12",
+          "900": "#7c3d12",
+          "950": "#432007",
+        },
+
+      },
+    },
+    container: {
+      center: true,
     },
   },
   plugins: [],
